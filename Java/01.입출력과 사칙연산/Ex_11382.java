a8610db95711b029e19f75c7655217b60a5f4a1b
pandahwang <pandahwang@naver.com>
2023-12-22 00:09:53 +0900

    commit

--- Java/01.입출력과 사칙연산/Ex_11382.java
+++ Java/01.입출력과 사칙연산/Ex_11382.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Ex_11382 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
-        
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
 
+        System.out.println(A+B+C);
     }
 }
--- Java/01.입출력과 사칙연산/Ex_2588.java
+++ Java/01.입출력과 사칙연산/Ex_2588.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
+import java.lang.reflect.Array;
 
 public class Ex_2588 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner (System.in);
+        Scanner sc = new Scanner(System.in);
+
         int A = sc.nextInt();
         int B = sc.nextInt();
 
         System.out.println(A*(B%10));
-        System.out.println(A*(B/10%10));
-        System.out.println(A*(B/100%10));
+        System.out.println(A*(B));
+        System.out.println(A*(B));
         System.out.println(A*B);
 
     }
